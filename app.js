e6202947c6578f9ff8aea54fb605b8860c7006a9
Andy Chen <andy@Andys-MacBook-Pro.local>
2018-10-29 12:58:05 -0700

    added thoughts and planning added helper function to assign numbers

--- app.js
+++ app.js
@@ -2,9 +2,27 @@
 listen for click event (edit)
 update text in local storage (with key)
 update display with new text value
+*/
 
 
- */
+//dynamically assigning keys
+var keyGen = function() {
+  var key = 0;
+
+  return function() {
+    key++;
+    return key;
+  };
+}
+
+var localeKeyGen = keyGen(); //have localStorage.setItem call this
+
+//or we can just name the key the value of user input
+//curKeyValue = $('#theKey').val();
+//only problem with that is, if the user inputs the same thing? that would not duplicate
+//
+
+
 
 $(document).ready(function(){
   console.log("before\n", window.localStorage);
@@ -14,7 +32,7 @@ $(document).ready(function(){
     $(".show-text").empty();
     var curTextValue = $('#theKey').val(); // reading from <input>
     var curKeyValue = "theKey"; // change to dynamic key?
-    localStorage.setItem(curKeyValue, curTextValue);
+    localStorage.setItem(curKeyValue + localeKeyGen(), curTextValue);
     $(".show-text").append(curTextValue);
   });
 
@@ -28,3 +46,24 @@ $(document).ready(function(){
   });
 
 });
+
+/*
+create individual items
+  we need unique key names so that key values will be stored to the local memory without overriding
+  iterate through local memory and log updated list on screen.
+delete individual items
+  remove the text from screen
+  can target unique key names and delete using localStorage.removeItem()
+  iterate through local memory and log updated list on screen.
+edit individual items
+  access the localmemory to edit the key holding the item you want to change and set a new value equal to it
+  iterate through local memory and log updated list on screen.
+helper functions
+  -a helper function which assigns a random number to theKey. Helps to give our local storage unique keys
+  -a helper function which loops through local memory and logs the values
+*/
+
+
+
+
+
