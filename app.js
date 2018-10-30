540d17e46c71fa0320e93b9bd0a45437a0fccf0f
Andy Chen <andy@Andys-MacBook-Pro.local>
2018-10-29 17:06:25 -0700

    added more planning decided to use the user input as the key also. added planning and idea to edit section

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ update display with new text value
 */
 
 
-//dynamically assigning keys
+//dynamically assigning keys //only problem is, when you refresh. closure variables is lost and count starts at 1
 var keyGen = function() {
   var key = 0;
 
@@ -22,26 +22,31 @@ var localeKeyGen = keyGen(); //have localStorage.setItem call this
 //only problem with that is, if the user inputs the same thing? that would not duplicate
 //
 
-//function which displays a refreshed feed with updated values from localMemory
+//function which displays a refreshed feed with updated values from localMemory by creating html elements
 var updateFeed = function(storage) {
   var length = storage.length;
   for (var i = 0; i < length; i++) {
-    console.log(localStorage.getItem(storage.key(i))); //APPEND THIS TO SHOWTEXT
+    var $itemDiv = $('<div class="item"></div>')     
+    $itemDiv.html(localStorage.getItem(storage.key(i)));
+    $(".show-text").append($itemDiv);
   }
 }
 
 
+
 $(document).ready(function(){
   console.log("before\n", window.localStorage);
-
+  updateFeed(localStorage);
+  console.log(window.localeStorage);
   // add event listener
   $(".add-text-btn").on("click", function(){
     $(".show-text").empty();
     var curTextValue = $('#theKey').val(); // reading from <input>
-    var curKeyValue = "theKey"; // change to dynamic key?
-    localStorage.setItem(curKeyValue + localeKeyGen(), curTextValue);
+    var curKeyValue = $('#theKey').val(); // change to dynamic key? reads user input also. no dupes only downside is, autosorts
+    localStorage.setItem(curKeyValue, curTextValue);
     console.log("after...", window.localStorage);
-    $(".show-text").append(curTextValue);
+    // $(".show-text").append(curTextValue);
+    updateFeed(localStorage);
   });
 
   // remove item from app
@@ -51,22 +56,28 @@ $(document).ready(function(){
     // clear local storage
     localStorage.clear();
     $(".show-text").empty();
+    updateFeed(localStorage);
   });
 
 });
 
 /*
-create individual items
+create individual items //done
   we need unique key names so that key values will be stored to the local memory without overriding
-  iterate through local memory and log updated list on screen.
+  iterate through local memory and log updated list on screen.  
+
 delete individual items
   remove the text from screen
   can target unique key names and delete using localStorage.removeItem()
   iterate through local memory and log updated list on screen.
+
 edit individual items
   access the localmemory to edit the key holding the item you want to change and set a new value equal to it
+  onclick popup box, enter new value;
   iterate through local memory and log updated list on screen.
-helper functions
+
+
+helper functions //done
   -a helper function which assigns a random number to theKey. Helps to give our local storage unique keys
       use a closure function and append to the curKeyVal.
   -a helper function which loops through local memory and logs the values
